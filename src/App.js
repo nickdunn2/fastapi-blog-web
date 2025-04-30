eb870c68b33f1402041745eb12fb2e29bc0bdd3c
Nick Dunn <nickdunn2@gmail.com>
2025-04-30 09:24:51 -0500

    feat: add basic data fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React, { useEffect, useState } from 'react'
+
+const BASE_URL = 'http://localhost:8000'
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/posts`)
+      .then(response => response.json())
+      .then(data => {
+        const reversedPosts = data.reverse()
+        setPosts(reversedPosts)
+      })
+      .catch(error => console.error('Error fetching posts:', error))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
