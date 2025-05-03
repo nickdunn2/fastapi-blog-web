d3f6d0f8b6de5eee779b87a43ce0a1f5029442b4
Nick Dunn <nickdunn2@gmail.com>
2025-05-03 15:34:42 -0500

    feat(posts): connect NewPost to API

--- src/NewPost.js
+++ src/NewPost.js
@@ -8,13 +8,84 @@ const NewPost = () => {
   const [imageUrl, setImageUrl] = useState('')
   const [creator, setCreator] = useState('')
 
+  const handleImageUpload = (e) => {
+    if (e.target.files[0]) {
+      setImageUrl(e.target.files[0])
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const formData = new FormData()
+    formData.append('image', imageUrl)
+
+    const requestOptions = {
+      method: 'POST',
+      body: formData,
+    }
+
+    fetch(`${BASE_API_URL}/posts/image`, requestOptions)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+
+        throw response
+      })
+      .then(data => {
+        createPost(data.filename)
+      })
+      .catch(error => {
+        console.error('Error uploading image: ', error)
+      })
+      .finally(() => {
+        // clean up the image upload input
+        setImageUrl(null)
+        document.getElementById('new-post-image-upload').value = null
+      })
+  }
+
+  const createPost = (imageUrl) => {
+    const jsonString = JSON.stringify({
+      title,
+      content,
+      image_url: imageUrl,
+      creator,
+    })
+    
+    const requestOptions = {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: jsonString,
+    }
+
+    fetch(`${BASE_API_URL}/posts`, requestOptions)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        
+        throw response
+      })
+      .then(data => {
+        window.location.reload()
+        window.scrollTo(0, 0)
+      })
+      .catch(error => {
+        console.error('Error creating post: ', error)
+      })
+  }
+
   return (
     <div className="new-post">
       <div className="new-post-image">
         <input 
           id="new-post-image-upload"
           type="file"
-          onChange={null}
+          onChange={handleImageUpload}
         />
       </div>
       <div className="new-post-creator">
@@ -51,7 +122,8 @@ const NewPost = () => {
         <button
           id="create-post-button"
           className="create-post-button"
-          onClick={null}
+          onClick={handleSubmit}
+          disabled={!imageUrl || !title || !content || !creator}
         >
           <span>Create Post</span>
         </button>
